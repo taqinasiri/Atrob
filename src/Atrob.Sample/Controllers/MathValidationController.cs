75785c6f6f25b4cce6f88cbdab367c266da11493
Taqi ツ <mtaqinasiria@gmail.com>
2023-10-14 23:57:37 +0330

    Add Divisibility Validation

---
+++ src/Atrob.Sample/Controllers/MathValidationController.cs
@@ -0,0 +1,14 @@
+﻿using Atrob.Validations.Math;
+
+namespace Atrob.Sample.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class MathValidationController : ControllerBase
+{
+    /// <summary>
+    /// Divisibility : 2 , 3
+    /// </summary>
+    [HttpPost]
+    public IActionResult Divisibility([Divisibility(2,3)] int number) => Ok();
+}
---
+++ src/Atrob.Test/Validations/MathValidationTests.cs
@@ -0,0 +1,21 @@
+﻿using Atrob.Validations.Math;
+
+namespace Atrob.Test.Validations;
+
+public class MathValidationTests
+{
+    [Theory]
+    [InlineData(true,10,2)]
+    [InlineData(true,15,5,6)]
+    [InlineData(false,8,3)]
+    [InlineData(false,17,5,3)]
+    public void Divisibility_Test(bool expectedResult,int value,params int[] numbersDivisible)
+    {
+        //arrange
+        var attribute = new DivisibilityAttribute(numbersDivisible);
+        //act
+        var isValid = attribute.IsValid(value);
+        //assert
+        Assert.Equal(expectedResult,isValid);
+    }
+}
